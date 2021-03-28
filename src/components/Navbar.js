43096111ef43b7bd6b8074c8778ff2d5d0bd8f91
Shaan Alam <shaanalam369@gmail.com>
2021-03-28 20:13:13 +0530

    navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <nav id="nav">
       <div className="container">
-        <ul>
+        <div
+          className={`nav__menuToggler ${navOpen ? "cross" : ""}`}
+          onClick={() => setNavOpen(!navOpen)}
+        >
+          <div></div>
+          <div></div>
+        </div>
+        <ul className={`${navOpen ? "visible" : ""}`}>
           <li>
             <a href="#nav" className="active">
               Home
