c243c2e15351fa77fce64000714b43307ef0458d
Shaan Alam <shaanalam369@gmail.com>
2021-03-28 11:18:55 +0530

    added animations to hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,51 @@
+import { motion } from "framer-motion";
+
+const HeroSection = () => (
+  <section className="home">
+    <div className="container">
+      <motion.img
+        src="/images/me.png"
+        alt="Shaan Alam"
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeIn", duration: 1 }}
+      />
+      <motion.h1
+        className="home__myName"
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeIn", duration: 1 }}
+      >
+        Shaan Alam
+      </motion.h1>
+      <motion.p
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeIn", duration: 1 }}
+      >
+        Hi there 👋️, I'm Shaan Alam. I am a Front-end Developer. I like to
+        create amazing stuffs using JavaScript, ReactJS and Firebase. See some
+        of my Projects below.
+      </motion.p>
+      <motion.div
+        className="home__actions"
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeIn", duration: 1.5 }}
+      >
+        <a href="#!" className="btn__primary">
+          Projects
+        </a>
+        <a
+          href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
+          className="btn__primary"
+          downlaod={true}
+        >
+          Download CV
+        </a>
+      </motion.div>
+    </div>
+  </section>
+);
+
+export default HeroSection;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,10 @@
+import HeroSection from "./HeroSection";
+
 const Home = () => {
   return (
-    <section className="home">
-      <div className="container">
-        <img src="/images/me.png" alt="Shaan Alam" />
-        <h1 className="home__myName">Shaan Alam</h1>
-        <p>
-          Hi there 👋️, I'm Shaan Alam. I am a Front-end Developer. I like to
-          create amazing stuffs using JavaScript, ReactJS and Firebase. See some
-          of my Projects below.
-        </p>
-        <div className="home__actions">
-          <a href="#!" className="btn__primary">
-            Projects
-          </a>
-          <a
-            href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
-            className="btn__primary"
-            downlaod={true}
-          >
-            Download CV
-          </a>
-        </div>
-      </div>
-    </section>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
