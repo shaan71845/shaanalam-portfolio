32f752380237f4eed3902ff3cab55959c9453dd9
Shaan Alam <shaanalam369@gmail.com>
2021-03-30 15:22:47 +0530

    fixed about me css issues

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,27 +7,29 @@ const AboutMe = () => {
 
   return (
     <section className="aboutme" id="about-me">
-      <div className="aboutme__leftContainer">
-        <div className="aboutme__text">
-          <h1>About me</h1>
-          <BlockContent
-            blocks={state?.author.about}
-            projectId="izpqy9cv"
-            dataset="production"
-          />
+      <div className="container">
+        <div className="aboutme__leftContainer">
+          <div className="aboutme__text">
+            <h1>About me</h1>
+            <BlockContent
+              blocks={state?.author.about}
+              projectId="izpqy9cv"
+              dataset="production"
+            />
+          </div>
+          <div className="aboutme__skills">
+            <h4>Skills</h4>
+            <img src="/images/html.svg" alt="HTML5" />
+            <img src="/images/css.svg" alt="CSS3" />
+            <img src="/images/sass.svg" alt="Scss" />
+            <img src="/images/javascript.svg" alt="JavaScript" />
+            <img src="/images/react.svg" alt="ReactJS" />
+            <img src="/images/firebase.svg" alt="Firebase" />
+          </div>
         </div>
-        <div className="aboutme__skills">
-          <h2>Skills I have</h2>
-          <img src="/images/html.svg" alt="HTML5"/>
-          <img src="/images/css.svg" alt="CSS3"/>
-          <img src="/images/sass.svg" alt="Scss"/>
-          <img src="/images/javascript.svg" alt="JavaScript"/>
-          <img src="/images/react.svg" alt="ReactJS"/>
-          <img src="/images/firebase.svg" alt="Firebase"/>
+        <div className="aboutme__rightContainer">
+          <img src={state?.author.image.asset.url} alt="Shaan Alam" />
         </div>
-      </div>
-      <div className="aboutme__rightContainer">
-        <img src={state?.author.image.asset.url} alt="Shaan Alam" />
       </div>
     </section>
   );
