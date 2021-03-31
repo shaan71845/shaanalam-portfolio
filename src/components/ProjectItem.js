1d1aa98f7d6228eb2efe68c8767aca599d2a488e
Shaan Alam <shaanalam369@gmail.com>
2021-03-31 16:39:16 +0530

    fixed projects grid

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,20 +5,18 @@ import Fade from "react-reveal/Fade";
 
 const ProjectItem = ({ project }) => {
   return (
-    <Fade bottom>
-      <Link to={`/project/${project.slug.current}`}>
-        <div className="project">
-          <div className="project__details">
-            <h3 className="project__title">{project.title}</h3>
-          </div>
-          <Img
-            src={project.mainImage.asset.url}
-            alt={project.title}
-            loader={<LinearProgress />}
-          />
+    <Link to={`/project/${project.slug.current}`}>
+      <div className="project">
+        <div className="project__details">
+          <h3 className="project__title">{project.title}</h3>
         </div>
-      </Link>
-    </Fade>
+        <Img
+          src={project.mainImage.asset.url}
+          alt={project.title}
+          loader={<LinearProgress />}
+        />
+      </div>
+    </Link>
   );
 };
 
