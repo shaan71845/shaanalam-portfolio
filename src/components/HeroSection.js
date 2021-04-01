dc934c06a1ca8d098b2b73e2f9c2eda1a66aa6aa
Shaan Alam <shaanalam369@gmail.com>
2021-04-01 16:09:55 +0530

    added social icons on hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,9 @@
 import Navbar from "./Navbar";
 import Fade from "react-reveal/Fade";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const HeroSection = () => {
   return (
@@ -7,33 +11,73 @@ const HeroSection = () => {
       <Navbar />
       <section className="hero__section" id="home">
         <div className="container">
-          <header>
+          <main>
+            <header>
+              <Fade>
+                <h4>Hi there 👋️, I'm</h4>
+              </Fade>
+              <Fade bottom>
+                <h1>Shaan Alam</h1>
+              </Fade>
+              <Fade bottom>
+                <p>
+                  I am a Front-end Developer. I like to create amazing stuffs
+                  using JavaScript. See some of my projects below!!
+                </p>
+              </Fade>
+            </header>
+            <Fade top>
+              <div className="hero__actions">
+                <a href="#projects" className="btn__primary">
+                  My Projects
+                </a>
+                <a
+                  href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
+                  className="btn__primary"
+                >
+                  Download CV
+                </a>
+              </div>
+            </Fade>
+          </main>
+          <div className="social-icons">
             <Fade>
-              <h4>Hi there 👋️, I'm</h4>
-            </Fade>
-            <Fade bottom>
-              <h1>Shaan Alam</h1>
-            </Fade>
-            <Fade bottom>
-              <p>
-                I am a Front-end Developer. I like to create amazing stuffs
-                using JavaScript. See some of my projects below!!
-              </p>
-            </Fade>
-          </header>
-          <Fade top>
-            <div className="hero__actions">
-              <a href="#projects" className="btn__primary">
-                My Projects
-              </a>
               <a
-                href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
-                className="btn__primary"
+                href="https://www.instagram.com/__shaanalam__/"
+                target="_blank"
+                rel="noreferrer"
               >
-                Download CV
+                <InstagramIcon />
               </a>
-            </div>
-          </Fade>
+            </Fade>
+            <Fade>
+              <a
+                href="https://www.linkedin.com/in/shaan-alam-01784018a/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LinkedInIcon />
+              </a>
+            </Fade>
+            <Fade>
+              <a
+                href="https://twitter.com/shaancodes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TwitterIcon />
+              </a>
+            </Fade>
+            <Fade>
+              <a
+                href="https://github.com/shaan71845"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GitHubIcon />
+              </a>
+            </Fade>
+          </div>
         </div>
       </section>
     </>
