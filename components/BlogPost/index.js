c8d6af7d0bf5ecefd56423149b3d23396046f7ee
Shaan Alam <shaanalam369@gmail.com>
2021-05-30 20:27:20 +0530

    fix: link issue

--- components/BlogPost/index.js
+++ components/BlogPost/index.js
@@ -117,7 +117,7 @@ const BlogPost = ({ post }) => {
                 delay: 0.1,
               }}
             >
-              <Link href={`/blog/${post.id}`} as="/blog/[slug]">
+              <Link href={`/blog/${post.id}`}>
                 <a>Read more...</a>
               </Link>
             </motion.div>
