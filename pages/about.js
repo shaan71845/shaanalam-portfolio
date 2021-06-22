3ca4e1dd08a112505599068f29c61c77996dc28d
Shaan Alam <shaanalam369@gmail.com>
2021-06-22 19:29:47 +0530

    added: about page hero section

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,64 @@
+import { useEffect } from "react";
 import withTransition from "../HOC/withTransition";
 import sanityClient from "../client";
+import styles from "../styles/About.module.scss";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Navbar from "../components/Navbar";
 
-const About = () => {
+const About = ({ about, experiences }) => {
+  const [headerRef, headerInView] = useInView();
+
+  const headerControls = useAnimation();
+
+  useEffect(() => {
+    if (headerInView) {
+      headerControls.start({
+        scaleX: 0,
+      });
+    }
+  }, [headerInView, headerControls]);
+
   return (
-    <div>
-      <div>
-        <div>
-          <p>hello</p>
+    <section className={styles.about}>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles["hero-content"]}>
+          <header className={styles.header}>
+            <h1>
+              <motion.div
+                className={styles["h1-inner"]}
+                initial={{ scaleX: 1 }}
+                animate={headerControls}
+                transition={{ ease: "easeInOut", duration: 0.5, delay: 0.8 }}
+                ref={headerRef}
+              ></motion.div>
+              Shaan Alam
+            </h1>
+          </header>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeInOut", delay: 1.2 }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium eligendi iure dolorem molestiae ipsum unde, cum, odio
+            porro excepturi odit ut minima assumenda recusandae facere eaque
+            magni totam pariatur. Iure.ima assumenda recusandae facere eaque
+            magni totam pariatur. Iure.ima assumenda recusandae facere eaque
+            magni totam pariatur. Iure.
+          </motion.p>
         </div>
+        <motion.div
+          className={styles.hero}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1, ease: "easeInOut" }}
+        >
+          <img src="/images/myelf.png" alt="Shaan Alam" />
+        </motion.div>
       </div>
-    </div>
+    </section>
   );
 };
 
@@ -35,20 +84,10 @@ export async function getStaticProps() {
     }
   `);
 
-  const education = await sanityClient.fetch(`
-    *[_type == "education"] {
-      institution,
-      course,
-      starting_year,
-      ending_year
-    }
-  `);
-
   return {
     props: {
       about,
       experiences,
-      education,
     },
   };
 }
