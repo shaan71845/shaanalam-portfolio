7c426162ce69f6f5a0f327d2e918362fcc9e8a88
Shaan Alam <shaanalam369@gmail.com>
2021-06-27 13:50:21 +0100

    moved footer styled components into footer.sc.js

---
+++ components/Footer/Footer.sc.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.footer`
+  border-top: 1px solid #ddd;
+  background: white;
+  padding: 2em;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  width: 90%;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 1em;
+
+  h3 {
+    font-family: "Alegreya", sans-serif;
+    color: rgba(0, 0, 0, 0.8);
+    margin-bottom: 1em;
+  }
+`;
+
+export const Logo = styled.div`
+  img {
+    width: 20%;
+  }
+`;
+
+export const FooterContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+  grid-gap: 2em;
+`;
+
+export const Col = styled.div`
+  ul {
+    list-style: none;
+
+    li {
+      margin-top: 1em;
+
+      a {
+        text-decoration: none;
+        color: #303030;
+        font-family: "Alegreya", sans-serif;
+      }
+    }
+  }
+
+  .social-links {
+    a {
+      margin-right: 10px;
+
+      svg {
+        transition: all 0.4s;
+        fill: rgba(0, 0, 0, 0.6);
+
+        &:hover {
+          fill: rgba(0, 0, 0, 0.8);
+        }
+      }
+    }
+  }
+`;
